62700deef080406eebbf24be1a2cd02ae6374c16
Wei Fuchunyang <1306701599@qq.com>
2024-03-10 15:33:51 +0800

    Add: 加入材质系统，粗糙度影响漫反射程度

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -64,14 +64,14 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y, glm::vec3 lightDir)
 	glm::vec3 color(0.0f);
 	float multiplier = 1.0f;
 
-	//光线反射两次
-	int bounces = 2;
+	//光线反射次数
+	int bounces = 5;
 	for (int i = 0; i < bounces; i++)
 	{
 		Renderer::HitPayload payload = TraceRay(ray);
 		if (payload.HitDistance < 0.0f)
 		{
-			glm::vec3 skyColor = glm::vec3(0.0f, 0.0f, 0.0f);
+			glm::vec3 skyColor = glm::vec3(0.6f, 0.7f, 0.9f);
 			color += skyColor * multiplier;
 			break;
 		}
@@ -81,14 +81,17 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y, glm::vec3 lightDir)
 		float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -LightDir), 0.0f); // == cos(angle)
 
 		const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
-		glm::vec3 sphereColor = sphere.Albedo;
+		const Material& material = m_ActiveScene->Materials[sphere.MaterialIndex];
+
+		glm::vec3 sphereColor = material.Albedo;
 		sphereColor *= lightIntensity;
 		color += sphereColor * multiplier;
 
-		multiplier *= 0.7f;
+		multiplier *= 0.5f;
 
 		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f;
-		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal);
+		ray.Direction = glm::reflect(ray.Direction,
+			payload.WorldNormal + material.Roughness * Walnut::Random::Vec3(-0.5f, 0.5f));
 	}
 
 	return glm::vec4(color, 1.0f);
